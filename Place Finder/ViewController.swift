8a0ba2bbb21365505e33819d2f4c0c1b6a191f77
Hsieh Shang-Fu <shawnhsieh@hsiehde-macbook-pro.local>
2018-04-14 18:47:21 -0700

    Initial Commit

---
+++ Place Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Place Finder
+//
+//  Created by Hsieh Shang-Fu on 4/14/18.
+//  Copyright © 2018 Hsieh Shang-Fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
